602cc66267a53925a45a7756a599bc96aec43cfa
Nathan Toone <nathan@toonetown.com>
2017-08-30 16:05:26 -0600

    Initial Checkin

---
+++ README.md
@@ -0,0 +1,31 @@
+## Quick Project Templates ##
+
+This project will create a shared Visual Studio / Xcode project files based off the templates in this directory.
+
+Any symbolic links that you want copied (i.e. `third_party`) should be absolute paths so the copy works correctly.
+
+_Note:_ The Boost library is already included in the project files, but no other third party libraries are included.
+
+
+### Setup ###
+
+Run the `deploy.sh` script to deploy to a new project.  The target location must **NOT** exist, but the parent directory of the target location **MUST** exist.
+
+
+### Creating Projects ###
+
+##### Visual Studio #####
+
+ 1.  Create new project (empty)
+ 2.  Change the `x86` configuration to `Win32`
+ 3.  Add the `defaults` property pages
+     - Add architecture-specific ones first, then Debug/Release, then the common one last
+ 4.  Add link libraries to `Linker | Input | Additional Dependencies` as `libmy_library$(THIRD_PARTY_LIBEXT)`
+
+
+##### Xcode #####
+
+ 1.  Create new project (empty)
+ 2.  Remove everything except `Product Name`
+ 3.  Add link libraries to `Other Linker Flags` as `-lmy_library`
+
---
+++ deploy.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+cd "$(dirname "${0}")"
+SOURCE="$(pwd)"
+cd ->/dev/null
+TARGET="${1}"
+
+[ -n "${TARGET}" ] || {
+    echo "Usage: ${0} [/path/to/poc_output]"
+    exit 1
+}
+[ ! -e "${TARGET}" ] || {
+    echo "Cannot copy to existing location ${TARGET}"
+    exit 1
+}
+[ -d "$(dirname "${TARGET}")" ] || {
+    echo "Directory $(dirname "${POC_DIR}") does not exist"
+    exit 1
+}
+
+TGT_NAME="$(basename "${TARGET}")"
+SRC_NAME="$(basename "${SOURCE}")"
+
+echo "Copying to '${TARGET}'..."
+cp -R "${SOURCE}" "${TARGET}" || exit $?
+
+echo "Cleaning up unneeded files..."
+rm -rf "${TARGET}/.git" "${TARGET}/README.md" "${TARGET}/$(basename "${0}")" || exit $?
+find "${TARGET}" -name ".DS_Store" -exec rm -f {} \;
+
+echo "Renaming files..."
+find "${TARGET}" -d -name "${SRC_NAME}.*" -print0 | while IFS= read -r -d '' _F; do
+    mv "${_F}" "$(echo "${_F}" | sed -e "s/${SRC_NAME}\(\.[^\.]*\)$/${TGT_NAME}\1/g")" || exit $?
+done
+
+echo "Update project name to ${TGT_NAME}..."
+find "${TARGET}" -type f -print0 | while IFS= read -r -d '' _F; do
+    sed -i '' -e "s/${SRC_NAME}/${TGT_NAME}/g" "${_F}" || exit $?
+done
+
+echo "Done!  POC deployed to '${TARGET}'"
+exit 0
